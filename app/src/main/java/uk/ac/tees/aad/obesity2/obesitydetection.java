43fcadba219de0a1ac48e2e60bedeac31f757228
Fattani Computers <amirraza2501@gmail.com>
2023-04-21 05:52:14 +0100

    implemeted the front end for the machine learning model

--- app/src/main/java/uk/ac/tees/aad/obesity2/obesitydetection.java
+++ app/src/main/java/uk/ac/tees/aad/obesity2/obesitydetection.java
@@ -3,12 +3,34 @@ package uk.ac.tees.aad.obesity2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class obesitydetection extends AppCompatActivity {
 
+
+    Spinner FamilyHistory, Smoke, Transport, Caec, Calc;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_obesitydetection);
+
+        FamilyHistory = findViewById(R.id.familyhistory);
+
+
+        ArrayAdapter<CharSequence> familydapter = ArrayAdapter.createFromResource(this,
+                R.array.familyhistory, android.R.layout.simple_spinner_item);
+        familydapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        FamilyHistory.setAdapter(familydapter);
+
+        FamilyHistory.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/uk/ac/tees/aad/obesity2/obesitydetectiontwo.java
@@ -0,0 +1,16 @@
+package uk.ac.tees.aad.obesity2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Spinner;
+
+public class obesitydetectiontwo extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_obesitydetectiontwo);
+    }
+}
