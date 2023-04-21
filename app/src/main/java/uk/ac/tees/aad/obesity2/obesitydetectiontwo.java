6450fac464ec666d3479d7897888fc795204ecd6
Fattani Computers <amirraza2501@gmail.com>
2023-04-22 00:47:43 +0100

    Singleton added for one network request

--- app/src/main/java/uk/ac/tees/aad/obesity2/obesitydetectiontwo.java
+++ app/src/main/java/uk/ac/tees/aad/obesity2/obesitydetectiontwo.java
@@ -1,5 +1,6 @@
 package uk.ac.tees.aad.obesity2;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,10 +9,23 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import  uk.ac.tees.aad.obesity2.NetworkRequest.Singleton;
 import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class obesitydetectiontwo extends AppCompatActivity {
 
@@ -23,12 +37,41 @@ public class obesitydetectiontwo extends AppCompatActivity {
     String caec;
     String calc;
     String Favcvalue;
-    FirebaseAuth auth;
+    public static String url = "https://obesitydetection.herokuapp.com/obese";
+    EditText etwter;
+
+    Button predict;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_obesitydetectiontwo);
         getSupportActionBar().hide();
+
+        etwter = findViewById(R.id.water);
+
+        SharedPreferences sh = getSharedPreferences("Prefs",MODE_PRIVATE);
+        String gender = sh.getString("gender","");
+        int age = sh.getInt("age",0);
+        float height = sh.getFloat("height",0f);
+        float weight = sh.getFloat("weight",0f);
+        predict = findViewById(R.id.predict);
+        predict.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+              /*  String water = etwter.getText().toString();
+                if(water.isEmpty())
+                {
+                    etwter.setError("Required");
+                    etwter.requestFocus();
+                    return;
+
+                }*/
+
+                Predict(GenderConveror(gender), String.valueOf(age),InchestoMeter(height), String.valueOf(weight)
+                ,familyhistory, Favcvalue,caec,smoke,etwter.getText().toString(),calc,transport);
+            }
+        });
 
 
 
@@ -69,9 +112,84 @@ public class obesitydetectiontwo extends AppCompatActivity {
              calc = currentbundle.getString("calc");
 
         }
-        Toast.makeText(obesitydetectiontwo.this,familyhistory + smoke + transport + caec + calc,Toast.LENGTH_LONG).show();
+        //Toast.makeText(obesitydetectiontwo.this,familyhistory + smoke + transport + caec + calc,Toast.LENGTH_LONG).show();
 
 
-        SharedPreferences sh = getSharedPreferences("Prefs",MODE_PRIVATE);
+
+
+
+
+
+
+    }
+
+    private void Predict(String genders, String Ages, String heights,String weights
+    , String Familyissues, String Favcs, String Caecs,String smokes
+    , String ch2O, String calcs, String tansportss) {
+
+
+        StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                Toast.makeText(obesitydetectiontwo.this,response.toString(),Toast.LENGTH_LONG).show();
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                Toast.makeText(obesitydetectiontwo.this, error.toString(),Toast
+                .LENGTH_LONG).show();
+                predict.setText(error.getMessage());
+
+            }
+        }){
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> detectiondata = new HashMap<>();
+                detectiondata.put("Gender",genders);
+                detectiondata.put("Age",Ages);
+                detectiondata.put("Height",heights);
+                detectiondata.put("Weight",weights);
+                detectiondata.put("family_history_with_overweight",Familyissues);
+                detectiondata.put("FAVC",Favcs);
+                detectiondata.put("FCVC","2.0");
+                detectiondata.put("CAEC",Caecs);
+                detectiondata.put("SMOKE",smokes);
+                detectiondata.put("CH2O",ch2O);
+                detectiondata.put("CALC",calcs);
+                detectiondata.put("MTRANS",tansportss);
+
+
+                return  detectiondata;
+            }
+
+
+        };
+
+        RequestQueue queue = Volley.newRequestQueue(obesitydetectiontwo.this);
+        queue.add(request);
+
+    }
+
+    public String GenderConveror(String gender)
+    {
+        String ConvertedGender="";
+        if(gender.equals("Male"))
+        {
+            ConvertedGender = "1";
+        }
+        if(gender.equals("Female"))
+        {
+            ConvertedGender = "0";
+        }
+
+        return ConvertedGender;
+    }
+    public String InchestoMeter(float hieght)
+    {
+        return  hieght/100 +"";
     }
 }
