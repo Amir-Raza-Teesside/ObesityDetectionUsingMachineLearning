6a83ebab1b37f344dc10de89a114ed461590b005
Fattani Computers <amirraza2501@gmail.com>
2023-05-09 13:04:46 +0100

    Adde the Splash Screen

--- app/src/main/java/uk/ac/tees/aad/obesity2/MainActivity.java
+++ app/src/main/java/uk/ac/tees/aad/obesity2/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 
@@ -15,14 +16,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = findViewById(R.id.button);
-
-        button.setOnClickListener(new View.OnClickListener() {
+        int SPLASH_TIME_OUT = 1500;
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Login.class);
-                startActivity(intent);
+            public void run() {
+                startActivity(new Intent(MainActivity.this, Login.class));
+                finish();
             }
-        });
+        }, SPLASH_TIME_OUT);
     }
 }
