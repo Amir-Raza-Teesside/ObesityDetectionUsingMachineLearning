ad086447d80d52b4519b8d5fc05237b0ae4b9d90
Fattani Computers <amirraza2501@gmail.com>
2023-05-22 09:37:53 +0100

    Added and updated the Splash Screen

--- app/src/main/java/uk/ac/tees/aad/obesity2/MainActivity.java
+++ app/src/main/java/uk/ac/tees/aad/obesity2/MainActivity.java
@@ -16,7 +16,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int SPLASH_TIME_OUT = 1500;
+        int SecondTIme;
+        int SPLASH_TIME_OUT = 2500;
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
